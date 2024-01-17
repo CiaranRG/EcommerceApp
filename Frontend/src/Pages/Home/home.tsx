89c286572fb85a6f0b6b46596b788bf2450af013
Ciaran Grant <ciarangrant2@aol.co.uk>
2024-01-17 18:26:59 +0000

    Homepage has a more defined layout and design

--- Frontend/src/Pages/Home/home.tsx
+++ Frontend/src/Pages/Home/home.tsx
@@ -3,17 +3,44 @@ import './home.scss'
 export default function Home(){
     return(
         <>
-        <div className="homeMainContent">
+        <main className="homeMainContent">
             <section className='homeSectionOne'>
-                
+                <div className='saleNowBanner'>
+                    <h1 className='saleNowText'>SALE NOW ON!</h1>
+                </div>
+                <div className='sectionOneContent'>
+                    <div className='shopNowBannerDiv'>
+                        <div className='shopNowBanner'>
+                            <h1>Newest Collection!</h1>
+                        </div>
+                    </div>
+                    <div className='sectionOneCards'>
+                        <div className='sectionOneItemOne'>
+                            <h1>Best Sellers!</h1>
+                        </div>
+                        <div className='sectionOneItemTwo'>
+                            <h1>Clearance!</h1>
+                        </div>
+                    </div>
+                </div>
             </section>
             <section className='homeSectionTwo'>
-
+                <div className='sectionTwoContent'>
+                    <h1>Shoes</h1>
+                    <div className='shoesButtons'>
+                        <a href='/shoes' className='getShoesButton'>Mens</a>
+                        <a href='/shoes' className='getShoesButton'>Womens</a>
+                        <a href='/shoes' className='getShoesButton'>Kids</a>
+                    </div>
+                </div>
             </section>
             <section className='homeSectionThree'>
-
+            <div className='sectionThreeContent'>
+                    <h1>Up To 50% Off</h1>
+                    <a href='/shoes' className='checkoutSalesButton'>Checkout Sales!</a>
+                </div>
             </section>
-        </div>
+        </main>
         </>
     )
 }
