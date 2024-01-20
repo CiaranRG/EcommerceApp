bfdfbf73a1c78fd6632acbd8259c7f0a814e90c6
Ciaran Grant <ciarangrant2@aol.co.uk>
2024-01-20 21:07:54 +0000

    Registration works

--- Backend/router/account.js
+++ Backend/router/account.js
@@ -3,18 +3,30 @@ import { config } from 'dotenv';
 import db from '../utils/databaseConnection.js';
 
 // Importing bcrypt to hash password and setting up our salt rounds
-// import bcrypt from 'bcryptjs'
+import bcrypt from 'bcryptjs'
 const saltRounds = 14;
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     console.log('Received Account Creation Request!')
-    const {email, username, password } = req.body
-    const formData = ({email, username, password})
+    const newAccount = req.body
     try {
         console.log('Try account creation')
-        res.status(201).json({message: 'Everything worked!'})
+        // If there was an error validation the scheme we throw this
+        // if (error){
+        //     throw new Error('Validation Error')
+        // }
+        // hashing the password on its own and saving it
+        const hash = await bcrypt.hash(newAccount.password, saltRounds,)
+
+        // Query to the database to insert these values into these columns
+        const result = await db.query(
+            'INSERT INTO users (email, username, password) VALUES ($1, $2, $3) RETURNING userId',
+            [newAccount.email, newAccount.username, hash]
+        )
+        console.log('Account Created!')
+        res.status(201).json({message: 'Data Submitted!'})
     } catch (err){
         console.log('Hit error on account creation')
         console.log(err)
