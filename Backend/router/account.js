f6cee7ef197e0f2f75d5f63cde239fd43ee3e673
Ciaran Grant <ciarangrant2@aol.co.uk>
2024-01-26 01:38:57 +0000

    setup changes

--- Backend/router/account.js
+++ Backend/router/account.js
@@ -19,7 +19,6 @@ router.post('/', async (req, res) => {
         // }
         // hashing the password on its own and saving it
         const hash = await bcrypt.hash(newAccount.password, saltRounds,)
-
         // Query to the database to insert these values into these columns
         const result = await db.query(
             'INSERT INTO users (email, username, password) VALUES ($1, $2, $3) RETURNING userId',
@@ -33,14 +32,25 @@ router.post('/', async (req, res) => {
     }
 })
 
-router.post('/login', (req, res) => {
+router.post('/login', async (req, res) => {
     console.log('Received Account Login Request!')
-    const {email, username, password } = req.body
-    const formData = ({email, username, password})
+    const loginAccount = req.body
     try {
         console.log('Try account Login')
+        const user = await db.query('SELECT * FROM users WHERE username = $1', [username])
+        if (user.rows.length === 0 ){
+            throw new Error('User Not Found')
+        }
+        const comparedPassword = bcrypt.compare(loginAccount.password, user.hash)
+        if (comparedPassword){
+
+        }   
+        // Send message to front end and create a session for them
         res.status(201).json({message: 'Everything worked!'})
     } catch (err){
+        if (err.message === 'User Not Found'){
+            res.status(404).json({message: err.message})
+        }
         console.log('Hit error on account Login')
         console.log(err)
     }
